f166ca5e4329b7dc34dc9086ea82330e9e02a28d
MonteDes <michaeli@fh-brandenburg.de>
2017-07-12 23:49:58 +0200

    added ContactList Activity + added READ_CONTACTS Permission in Manifest

---
+++ app/src/main/java/de/thb/fbi/maus/bm/login/ContactList.java
@@ -0,0 +1,13 @@
+package de.thb.fbi.maus.bm.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
